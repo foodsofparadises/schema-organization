8a1e4cd5203dd9fa2f4fc70c6332ea36dabfb0e2
foodsofparadise <79972418+foodsofparadise@users.noreply.github.com>
2021-03-03 16:52:31 +0700

    Add files via upload

---
+++ Schema-Person.js
@@ -0,0 +1,25 @@
+<script type="application/ld+json">
+{
+  "@context": "http://schema.org/",
+  "@type": "Person",
+  "name": "Lưu thị Diễm Hương",
+  "alternateName": "Lưu thị Diễm Hương",
+  "url": "https://vincitytaymodaimo.vn/author/duongthanhcuong/",
+  "image": "https://vincitytaymodaimo.vn/wp-content/uploads/2019/01/avtar.jpg",
+  "description": "Tôi là Dương Thanh Cường , tôi có 3 năm kinh nghiệm trong lĩnh vực truyền thông bất động sản. Hiện tại tôi đang là quản lý website dự án chung cư Vincity Sportia Tây Mỗ Đại Mỗ Hà Nội.",
+  "sameAs": [
+    "https://vi.gravatar.com/luuthidiemhuong9x",
+	"https://twitter.com/luthidiemhuong",
+    "https://plus.google.com/102338901739819250270",
+    "https://www.youtube.com/channel/UCxpyAgtnwyr8IsZXiHVZGLg/",
+    "www.linkedin.com/in/luuthidiemhuong",
+    "hhttps://about.me/luuthidiemhuong",
+    "http://www.brandsvietnam.com/marketer/luuthidiemhuong"
+  ],
+  "jobTitle": "Content Writer",
+  "worksFor": {
+    "@type": "Organization",
+    "name": "Gấu Bông Đẹp Giá Rẻ ở Hà Nội | Shop Gấu Cao Cấp | Teddy.vn"
+  }  
+}
+</script>
